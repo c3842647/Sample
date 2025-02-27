dbb15ba4184b61b0c5f59cf3f3e66c0f2fd59425
c3842647 <c3842647@gmail.com>
2025-02-27 13:00:17 -0500

    Update xss_stored.py

--- vulns/xssinjection/xss_stored.py
+++ vulns/xssinjection/xss_stored.py
@@ -13,3 +13,16 @@ def xss_stored_api(request, app):
     result = app.db_helper.execute_write('INSERT INTO messages (message) VALUES (:msg)', { 'msg': message })
 
     return xss_stored_page(request, app)
+
+def xss_stored_page(request, app):
+    messages = app.db_helper.execute_read('SELECT * FROM messages', {})
+    messages = list(map(lambda it: it[0], messages))
+
+    return render_template('xss-stored.html', messages=messages)
+
+
+def xss_stored_api(request, app):
+    message = request.form['message']
+    result = app.db_helper.execute_write('INSERT INTO messages (message) VALUES (:msg)', { 'msg': message })
+
+    return xss_stored_page(request, app)
